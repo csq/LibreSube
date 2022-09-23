7a0af315190c1bcb593d509527b902cd4a26ae93
Carlos Quiroz <carlos.quiroz.90@gmail.com>
2022-09-23 15:03:15 -0300

    add: leaflet map with layer IGN

--- src/views/TasView.vue
+++ src/views/TasView.vue
@@ -1,5 +1,45 @@
 <template>
   <div class="tas">
-    <h1>This is an new page</h1>
+    <div id="mapContainer"></div>
   </div>
 </template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+//import dataset from "@/datasets/Terminales Automáticas SUBE/sube_terminales_autoservicio_activas_2019-10-01.geojson";
+
+export default {
+  name: "TasView",
+  data() {
+    return{
+      center: [-41, -60]
+    }
+  },
+  
+  methods: {
+    setupLeafletMap: function () {
+      const mapDiv = L.map("mapContainer").setView(this.center, 4);
+      L.tileLayer(
+        'https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/capabaseargenmap@EPSG%3A3857@png/{z}/{x}/{-y}.png',
+        {
+          attribution:
+          '<a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a> | <a href="http://www.ign.gob.ar/AreaServicios/Argenmap/IntroduccionV2" target="_blank">Instituto Geográfico Nacional</a> + <a href="http://www.osm.org/copyright" target="_blank">OpenStreetMap</a>',
+          minZoom: 3,
+          maxZoom: 18,
+       }
+      ).addTo(mapDiv);
+      //L.geoJSON(dataset).addTo(mapDiv);
+    },
+  },
+  mounted() {
+    this.setupLeafletMap();
+  },
+};
+</script>
+
+<style scoped>
+#mapContainer {
+  height: 550px;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.geojson$/,
+          loader: 'json-loader'
+        }
+      ]
+    }
+  }
+}
