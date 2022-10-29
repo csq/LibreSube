a612cce54f29d997ce0a861542ea0d89d6c0a372
Carlos Quiroz <carlos.quiroz.90@gmail.com>
2022-10-29 19:08:17 -0300

    fix: footer now in bottom position

--- src/components/FooterSegment.vue
+++ src/components/FooterSegment.vue
@@ -1,26 +1,30 @@
 <template>
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
-<div class="fixed-bottom">
-  <footer>
-    <a href="#"><i class="fa fa-github"></i></a>
-    <a href="#"><i class="fa fa-envelope-o"></i></a>
-  </footer>
-</div>
+<footer class="footer mt-auto">
+  <div class="container pt-1">
+    <div class="row">
+      <div class="col">
+        <a href="#" target="_blank"><i class="fa fa-github"></i></a>
+        <a href="#" target="_blank"><i class="fa fa-envelope-o"></i></a>
+      </div>
+    </div>
+  </div>
+</footer>
 </template>
 
-<style>
+<style scoped>
 footer {
   width: 100%;
-  background: #484946;
+  background: #333333;
   text-align: center;
 }
 
 footer a {
   color: #ffffff;
   font-size: 20px;
-  padding: 20px;
+  padding-left: 1em;
+  padding-right: 1em;
   transition: all .5s ease;
-  
 }
 
 footer a:hover {
