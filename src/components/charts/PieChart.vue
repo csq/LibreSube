b8f6800020a1bfa8cd7ff3ddbb6effc881b32ef5
Carlos Quiroz <carlos.quiroz.90@gmail.com>
2022-11-01 14:03:04 -0300

    add: dropdown-menu to PieChart and its corresponding visualization

--- src/components/charts/PieChart.vue
+++ src/components/charts/PieChart.vue
@@ -11,6 +11,18 @@
     :width="width"
     :height="height"
   />
+  <div class="dropdown">
+  <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Año
+  </button>
+  <ul class="dropdown-menu dropdown-menu-dark">
+    <li><a class="dropdown-item selected" v-on:click="getData(2019)">2019</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" v-on:click="getData(2018)">2018</a></li>
+    <li><a class="dropdown-item" v-on:click="getData(2017)">2017</a></li>
+    <li><a class="dropdown-item" v-on:click="getData(2016)">2016</a></li>
+  </ul>
+</div>
 </template>
   
 <script>
@@ -19,26 +31,10 @@ import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale } f
 
 import data from "@/datasets/Info SUBE/cancelaciones_ano_modo.csv";
 
+// Arrays con los datos del csv
 var bus = new Array();
 var subway = new Array();
 var train = new Array();
-
-var año = '2019'
-
-const regex = new RegExp(año);
-
-for (let i = 0; i < data.length; i++) {
-
-  if (regex.test(data[i].anio)) {
-    if (data[i].MODO == "COLECTIVO") {
-      bus.push(data[i].TOTAL)
-    }else if (data[i].MODO == "SUBTE") {
-      subway.push(data[i].TOTAL)
-    } else {
-      train.push(data[i].TOTAL)
-    }
-  }
-}
 
 ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale)
   
@@ -75,9 +71,10 @@ export default {
       default: () => {}
     }
   },
+  // Configuración PieChart
   data() {
     return {
-      PieChartTitle: 'Usuarios anuales por modo de transporte',
+      PieChartTitle: null,
       chartData: {
         labels: [ 'Colectivo', 'Tren', 'Subte' ],
         datasets: [ {
@@ -87,10 +84,48 @@ export default {
       },
       chartOptions: {
         responsive: true,
-        maintainAspectRatio: false,
-        animation: true
+        maintainAspectRatio: false
       }
     }
+  },
+  methods: {
+    // Extrae los datos del csv y los almacena en arrays
+    getData: function(year) {
+
+      if (bus.length != 0) {
+        bus.length = 0;
+        subway.length = 0;
+        train.length = 0;
+      }
+
+      const regex = new RegExp(year);
+
+      for (let i = 0; i < data.length; i++) {
+
+        if (regex.test(data[i].anio)) {
+          if (data[i].MODO == "COLECTIVO") {
+            bus.push(data[i].TOTAL)
+          }else if (data[i].MODO == "SUBTE") {
+            subway.push(data[i].TOTAL)
+          } else {
+            train.push(data[i].TOTAL)
+          }
+        }
+
+      }
+      // Establece en título del chart según el año seleccionado
+      this.PieChartTitle = 'Usuarios en '+year+' por modo de transporte';
+      
+      this.updateData();
+    },
+    // Actualiza los datos del chart para que se visualizen
+    updateData: function() {
+      this.chartData.datasets[0].data = [bus, train, subway];
+    }
+  },
+  // Ejecuta la funcion getData al cargar la pagina
+  created: function() {
+    this.getData('2019');
   }
 }
 </script>
