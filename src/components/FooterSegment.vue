dbf1b720d5d8af032d88d0566ff9ce21e5f780ed
Carlos Quiroz <carlos.quiroz.90@gmail.com>
2022-11-07 15:09:32 -0300

    add: link to repository github

--- src/components/FooterSegment.vue
+++ src/components/FooterSegment.vue
@@ -1,11 +1,11 @@
 <template>
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
 <footer class="footer mt-auto">
-  <div class="container pt-1">
+  <div class="container p-1">
     <div class="row">
       <div class="col">
-        <a href="#" target="_blank"><i class="fa fa-github"></i></a>
-        <a href="#" target="_blank"><i class="fa fa-envelope-o"></i></a>
+        <a href="https://github.com/csq/libre-sube/" target="_blank"><i class="fa fa-github"></i></a>
+        <a href="mailto:carlos.quiroz.90@gmail.com"><i class="fa fa-envelope-o"></i></a>
       </div>
     </div>
   </div>
@@ -13,21 +13,21 @@
 </template>
 
 <style scoped>
-footer {
+.footer {
   width: 100%;
   background: #333333;
   text-align: center;
 }
 
-footer a {
+.footer a {
   color: #ffffff;
-  font-size: 20px;
+  font-size: large;
   padding-left: 1em;
   padding-right: 1em;
   transition: all .5s ease;
 }
 
-footer a:hover {
+.footer a:hover {
   color: #0e8ccc;
 }
 </style>
